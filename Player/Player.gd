dc6d369e9ad8cec449c4484bf5e383d704e84b96
karl <karl.bittner@hexaquo.at>
2021-08-05 22:29:52 +0200

    Bugfixes for allowing rotating movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -22,14 +22,19 @@ func get_next_rails():
 	return _get_colliding_rails_of_area($FutureRailsArea)
 
 
-func _set_position_for_rails(rails_node):
+func _get_position_for_rails(rails_node, interpolate_position):
 	var curve = rails_node.get_node("MovementPath").curve
+	var interpolated_point = curve.interpolate_baked(interpolate_position * curve.get_baked_length())
 	
-	position = rails_node.position + curve.interpolate_baked(current_rails_progress * curve.get_baked_length())
+	return rails_node.position + interpolated_point.rotated(rails_node.rotation)
 
 
 func _process(delta):
-	# For the first launch: get the current rail as soon as it's available; if it isn't, return
+	if current_rails_progress >= 1.0:
+		current_rails = get_next_rails()
+		current_rails_progress -= 1.0
+	
+	# For the first launch (and end): get the current rail as soon as it's available; if it isn't, return
 	# (For some reason it can take 1 frame until the rail is found)
 	if not current_rails:
 		current_rails = get_current_rails()
@@ -37,10 +42,8 @@ func _process(delta):
 		if not current_rails:
 			return
 	
-	if current_rails_progress >= 1.0:
-		current_rails = get_next_rails()
-		current_rails_progress -= 1.0
-	
-	_set_position_for_rails(current_rails)
+	# Update position and rotation
+	position = _get_position_for_rails(current_rails, current_rails_progress)
+	rotation = position.angle_to_point(_get_position_for_rails(current_rails, current_rails_progress + 0.1))
 	
 	current_rails_progress += delta
