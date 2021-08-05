fe1e22d2b80435a14ecc594b60dc24456447454c
karl <karl.bittner@hexaquo.at>
2021-08-05 22:00:57 +0200

    Refactor Player

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,37 +1,44 @@
 extends Node2D
 
-var current_rail: Node2D
-var next_rail: Node2D
+var current_rails: Node2D
+var next_rails: Node2D
 
-var current_rail_progress := 0.0
+var current_rails_progress := 0.0
 
 
-func get_current_rail():
-	var all_areas = $CurrentRailsArea.get_overlapping_areas()
+func _get_colliding_rails_of_area(area: Area2D):
+	var all_areas = area.get_overlapping_areas()
 	
 	for area in all_areas:
 		if area.get_parent().is_in_group("Rails"):
-			current_rail = area.get_parent()
-	
+			return area.get_parent()
 
-func get_next_rail():
-	var all_areas = $FutureRailsArea.get_overlapping_areas()
-	
-	for area in all_areas:
-		print(area.get_parent().name)
-		if area.get_parent().is_in_group("Rails"):
-			current_rail = area.get_parent()
+
+func get_current_rails():
+	return _get_colliding_rails_of_area($CurrentRailsArea)
+
+
+func get_next_rails():
+	return _get_colliding_rails_of_area($FutureRailsArea)
+
+
+func _set_position_for_rails(rails_node):
+	position = rails_node.position + rails_node.get_node("MovementPath").curve.interpolate(0, current_rails_progress)
 
 
 func _process(delta):
-	# Mainly for the first launch
-	if not current_rail:
-		get_current_rail()
+	# For the first launch: get the current rail as soon as it's available; if it isn't, return
+	# (For some reason it can take 1 frame until the rail is found)
+	if not current_rails:
+		current_rails = get_current_rails()
+		
+		if not current_rails:
+			return
 	
-	position = current_rail.position + current_rail.get_node("MovementPath").curve.interpolate(0, current_rail_progress)
+	if current_rails_progress >= 1.0:
+		current_rails = get_next_rails()
+		current_rails_progress -= 1.0
 	
-	current_rail_progress += delta
-
-	if current_rail_progress >= 1.0:
-		get_next_rail()
-		current_rail_progress -= 1.0
+	_set_position_for_rails(current_rails)
+	
+	current_rails_progress += delta
